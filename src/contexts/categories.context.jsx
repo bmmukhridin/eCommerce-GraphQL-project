8e90de92076b78abb363da916bcc695017e922bf
bmmukhridin <bmmukhridin@icloud.com>
2022-10-15 16:16:30 -0700

    added GraphQl

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -8,33 +8,34 @@ export const CategoriesContext = createContext({
 const COLLECTIONS = gql`
   query GetCollections {
     collections {
-      id,
-      title,
-      items{
-        id,
-        name,
-        price,
+      id
+      title
+      items {
+        id
+        name
+        price
         imageUrl
       }
     }
   }
-`
+`;
 ///this is testing #2
 export const CategoriesProvider = ({ children }) => {
-  const {loading, error, data} = useQuery(COLLECTIONS)
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log(data)
-useEffect(()=>{
-  if(data){
-    const {collection} =data;
-    const collectionMap = collection.reduce((acc, collection)=>{
-      const {title, item} = collection;
-      acc[title.lowerCase()] = item;
-      return acc;
-    })
-  }
-},[data])
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+      setCategoriesMap(collectionMap);
+    }
+  }, [data]);
+
   const value = { categoriesMap };
   return (
     <CategoriesContext.Provider value={value}>
